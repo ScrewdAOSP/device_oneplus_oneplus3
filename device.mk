bd35be4209c860b3e21dbad3e6d3e931a5931cc6
Luca Stefani <luca020400@lineageos.org>
2017-09-26 00:07:29 -0400

    Update audio for O
    * Build audio HIDL HALs
    * Remove dead packages
    * Update audio flags for AOSP HAL
    
    Change-Id: Ide624d3008739b68a1b4cac347237e72cd4b4b20

--- device.mk
+++ device.mk
@@ -108,6 +108,9 @@ PRODUCT_BOOT_JARS += \
 # Audio
 PRODUCT_PACKAGES += \
     audiod \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.soundtrigger@2.0-impl \
     audio.a2dp.default \
     audio.primary.msm8996 \
     audio.r_submix.default \
